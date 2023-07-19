022be3ff703a48f3545b7dfce9d896ba12504bc2
Vera1S <128926912+Vera1S@users.noreply.github.com>
2023-07-19 09:37:30 -0400

    Add files via upload
    Задание 1.
    Создайте четыре переменные: типа "byte", типа "int", типа "double", типа "float".
    Заполните все четыре переменные произвольными(на ваше усмотрение) значениями.
    С этим набором переменных сделайте преобразования типа "cужение" и "расширение".
    Запишите результаты преобразований в переменные соответствующего типа.
    Выведите результаты преобразований на экран с помощь метода System.out.println().
    Задание 2
    Напишите программу, которая спрашивает имя пользователя, а потом выводит строку  “Привет {имя пользователя}!” на  экран.
    На месте {имя пользователя} должно быть введённое пользователем имя.

---
+++ ScannerH_W.java
@@ -0,0 +1,16 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class ScannerH_W {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How are you! What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Yout name is: " + name);
+    }
+}
+
