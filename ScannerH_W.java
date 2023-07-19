de780c2c50fc41ef0c33ae0c64c45859a415d3be
Vera1S <128926912+Vera1S@users.noreply.github.com>
2023-07-19 10:29:46 -0400

    Update ScannerH_W.java
    Только Scanner

--- ScannerH_W.java
+++ ScannerH_W.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class ScannerH_W {
     public static void main(String[] args) {
+       
+// Задание 2
+// Напишите программу, которая спрашивает имя пользователя, а потом выводит строку  “Привет {имя пользователя}!” на  экран.
+// На месте {имя пользователя} должно быть введённое пользователем имя.
         Scanner scanner = new Scanner(System.in);
         System.out.print("How are you! What is your name? ");
         String name = scanner.nextLine();
